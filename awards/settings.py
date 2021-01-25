fccad4d805a83132aea5bb3bfe4b0ddc6d352195
Mariga123 <johnmariga8@gmail.com>
2021-01-25 18:18:02 +0300

    create and connect to database

--- awards/settings.py
+++ awards/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ratings',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,10 @@ WSGI_APPLICATION = 'awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ratings',
+        'USER': 'moringa',
+    'PASSWORD':'zelly',
     }
 }
 
